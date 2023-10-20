7632be2096b3350b240303919c69530a1a0dd9b9
ari-mhp <103044712+ari-mhp@users.noreply.github.com>
2023-10-20 16:52:57 +0700

    add sorting character

--- sort-array.c
+++ sort-array.c
@@ -2,42 +2,55 @@
 // #include <string.h>
 #include <ctype.h>
 
+void initNumArray(int arrayNum[], int arraySize);
+void initCharArray(char arrayChar[], int arraySize);
+
 void sortNumAsc(int arrayNum[], int size);
 void sortNumDesc(int arrayNum[], int size);
-void printArray(int arrayNum[], int size, char sortBy);
+
+void sortCharAsc(char arrayChar[], int size);
+void sortCharDesc(char arrayChar[], int size);
+
+void printNumArray(int arrayNum[], int size, char sortBy);
+void printCharArray(char arrayChar[], int size, char sortBy);
 
 int main()
 {
     int arraySize;
+    char arrayType;
+    char sortBy;
 
     do
     {
-        printf("Insert arrayNum size: ");  // insert array size greater than 0
+        printf("Insert array size: "); // insert array size greater than 0
         scanf("%d", &arraySize);
-        scanf("%c");
     } while (arraySize <= 0);
 
-
-
     int arrayNum[arraySize];
-    int elementInput;
-    char sortBy;
+    char arrayChar[arraySize];
 
-    // Insert element array
-    for (int j = 0; j < arraySize; j++)
+    // Insert array type
+    do
     {
-        printf("Insert element #%d: ", j);
-        scanf("%d", &elementInput);
-        arrayNum[j] = elementInput;
-    }
+        printf("Insert array type Numeric(N)/Char(C): ");
+        scanf("%c"); // clean input buffer
+        scanf("%c"); // clean input buffer
+        scanf("%c", &arrayType);
+        arrayType = toupper(arrayType);
+        if (arrayType == 'N' || arrayType == 'C')
+        {
+            break;
+        }
+    } while (arrayType != 'N');
 
-    // Print inserted array
-    printf("Array: [ ");
-    for (int i = 0; i < arraySize; i++)
+    if (arrayType == 'N')
     {
-        printf("%d ", arrayNum[i]);
+        initNumArray(arrayNum, arraySize);
     }
-    printf("]");
+    else if (arrayType == 'C')
+    {
+        initCharArray(arrayChar, arraySize);
+    }
 
     // Insert sorting type
     do
@@ -57,18 +70,85 @@ int main()
     switch (sortBy)
     {
     case 'A':
-        sortNumAsc(arrayNum, arraySize);
+        if (arrayType == 'N')
+        {
+            sortNumAsc(arrayNum, arraySize);
+        }
+        else
+        {
+            sortCharAsc(arrayChar, arraySize);
+        }
         break;
     case 'D':
-        sortNumDesc(arrayNum, arraySize);
+        if (arrayType == 'N')
+        {
+            sortNumDesc(arrayNum, arraySize);
+        }
+        else
+        {
+            sortCharDesc(arrayChar, arraySize);
+        }
         break;
     default:
         break;
     }
 
-    printArray(arrayNum, arraySize, sortBy);
+    if (arrayType == 'N')
+    {
+        printNumArray(arrayNum, arraySize, sortBy);
+    }
+    else
+    {
+        printCharArray(arrayChar, arraySize, sortBy);
+    }
 
     return 0;
+}
+
+void initNumArray(int arrayNum[], int arraySize)
+{
+
+    int elementInput;
+
+    // Insert element array
+    for (int j = 0; j < arraySize; j++)
+    {
+        printf("Insert element #%d: ", j);
+        scanf("%d", &elementInput);
+        arrayNum[j] = elementInput;
+    }
+
+    // Print inserted array
+    printf("> Array: [ ");
+    for (int i = 0; i < arraySize; i++)
+    {
+        printf("%d ", arrayNum[i]);
+    }
+    printf("]");
+}
+
+void initCharArray(char arrayChar[], int arraySize)
+{
+
+    int elementInput;
+
+    // Insert element array
+    for (int j = 0; j < arraySize; j++)
+    {
+        printf("Insert element #%d: ", j);
+        scanf("%c");
+        scanf("%c");
+        scanf("%c", &elementInput);
+        arrayChar[j] = elementInput;
+    }
+
+    // Print inserted array
+    printf("> Array: [ ");
+    for (int i = 0; i < arraySize; i++)
+    {
+        printf("%c ", arrayChar[i]);
+    }
+    printf("]");
 }
 
 void sortNumAsc(int arrayNum[], int size)
@@ -105,20 +185,71 @@ void sortNumDesc(int arrayNum[], int size)
     }
 }
 
-void printArray(int arrayNum[], int size, char sortBy)
+void sortCharAsc(char arrayChar[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    { // outer loop untuk tiap elemen di array (e1, e2, e3, ... , dst)
+        for (int j = 0; j < size - i - 1; j++)
+        { // inner loop untuk perbandingan dengan elemen di sebelahnya
+            if (arrayChar[j] > arrayChar[j + 1])
+            {
+                int temp = arrayChar[j];
+                arrayChar[j] = arrayChar[j + 1];
+                arrayChar[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void sortCharDesc(char arrayChar[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    { // outer loop untuk tiap elemen di array (e1, e2, e3, ... , dst)
+        for (int j = 0; j < size - i - 1; j++)
+        { // inner loop untuk perbandingan dengan elemen di sebelahnya
+            if (arrayChar[j] < arrayChar[j + 1])
+            {
+                int temp = arrayChar[j];
+                arrayChar[j] = arrayChar[j + 1];
+                arrayChar[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void printNumArray(int arrayNum[], int size, char sortBy)
 {
     if (sortBy == 'A')
     {
-        printf("Sorted arrayNum (Ascending): [ ");
+        printf("> Sorted array (Ascending): [ ");
     }
     else
     {
-        printf("Sorted arrayNum (Descending): [ ");
+        printf("> Sorted array (Descending): [ ");
     }
 
     for (int i = 0; i < size; i++)
     {
         printf("%d ", arrayNum[i]);
+    }
+
+    printf("]");
+}
+
+void printCharArray(char arrayChar[], int size, char sortBy)
+{
+    if (sortBy == 'A')
+    {
+        printf("> Sorted array (Ascending): [ ");
+    }
+    else
+    {
+        printf("> Sorted array (Descending): [ ");
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%c ", arrayChar[i]);
     }
 
     printf("]");
