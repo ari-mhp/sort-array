ba837243a7f5b51da9524cca6fafceb37af134c6
ari-mhp <103044712+ari-mhp@users.noreply.github.com>
2023-10-20 14:11:49 +0700

    init

---
+++ sort-array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+void sort(int array[], int size) {
+    for (int i = 0; i < size - 1; i++) {            // outer loop untuk tiap elemen di array (e1, e2, e3, ... , dst)
+        for (int j = 0; j < size - 1; j++) {        // inner loop untuk perbandingan dengan elemen di sebelahnya
+            if (array[j] > array[j + 1]) {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int array[], int size) {
+    for(int i = 0; i < size; i ++) {
+        printf("%d ", array[i]);
+    }
+}
+
+int main() {
+
+    int array[] = {0, 9, 1, 8, 2, 7, 3, 6, 4, 5};
+    int size = sizeof(array) / sizeof(array[0]);
+
+    sort(array, size);
+
+    printArray(array, size);
+
+    return 0;
+}
